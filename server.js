328cb8f1d742cc44732c3b9f2bf3659a17ca8938
LuzianeFreitas <freitas.lu@outlook.com>
2020-09-08 12:07:52 -0300

    Update server

--- server.js
+++ server.js
@@ -1,20 +1,30 @@
 // importando o express
 const express = require('express');
 const mongoose = require('mongoose');
+const requireDir = require('require-dir');
 // iniciando o app
 const app = express();
 
 // Iniciando o db
-mongoose.connect('mongodb://localhost:27017/nodeapi', { useUnifiedTopology: true, useNewUrlParser: true });
+mongoose.connect(
+    'mongodb://localhost:27017/nodeapi',
+     { useUnifiedTopology: true, useNewUrlParser: true }
+);
+requireDir('./src/models');
 
-
-
+const Product = mongoose.model('Product');
 
 // Primeira rota
 // req: contem todos os detalhes e informações da requisição
 // res: resposta da requisição feita
 app.get('/', (req, res) => {
-    res.send('Hello Rocketseat');
+    Product.create({
+        title: 'React Native',
+        description: 'Build native apps with React',
+        url: 'http://github.com/facebook/react-native'
+    });
+    
+    return res.send('Hello Rocketseat');
 });
 
 // aplicação estara ouvindo a porta 3001 do navegador
