793e22b76c7978172b7d0d1ac3d640af029b52e0
LuzianeFreitas <freitas.lu@outlook.com>
2020-09-08 11:27:06 -0300

    Added mongodb

--- server.js
+++ server.js
@@ -1,8 +1,16 @@
 // importando o express
 const express = require('express');
-
+const mongoose = require('mongoose');
+// iniciando o app
 const app = express();
 
+// Iniciando o db
+mongoose.connect('mongodb://localhost:27017/nodeapi', { useUnifiedTopology: true, useNewUrlParser: true });
+
+
+
+
+// Primeira rota
 // req: contem todos os detalhes e informações da requisição
 // res: resposta da requisição feita
 app.get('/', (req, res) => {
