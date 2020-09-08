be3337b4a4ed78456eeb63413e092fcb305482a5
LuzianeFreitas <freitas.lu@outlook.com>
2020-09-08 13:42:35 -0300

    Added functions

--- src/routes.js
+++ src/routes.js
@@ -4,5 +4,10 @@ const ProductController = require('./controler/ProductController');
 // req: contem todos os detalhes e informações da requisição
 // res: resposta da requisição feita
 routes.get('/products', ProductController.index);
+routes.get('/products/:id', ProductController.show);
+routes.post('/products', ProductController.store);
+routes.put('/products/:id', ProductController.update);
+routes.delete('/products/:id', ProductController.destroy);
+
 
 module.exports = routes;
